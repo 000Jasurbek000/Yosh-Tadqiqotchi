496900d8a600b03eef0fac7b9f5536884f8925bc
Jasurbek Davletov <davletovjasurbek22@gmail.com>
2026-03-04 20:41:43 +0500

    update

--- xalikova_project/settings.py
+++ xalikova_project/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'xalikova_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "yoshtaqq_yoshtadqiqotchiDb",
+        "USER": "yoshtaqq_xalikova",
+        "PASSWORD": "Umida_3087",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
